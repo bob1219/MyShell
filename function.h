e40278a70c8a5dc0e3917ab291706d7951bf6a7e
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-27 11:15:05 +0900

    Addition declarations of command functions

--- function.h
+++ function.h
@@ -18,6 +18,26 @@ namespace myshell
 	{
 		std::cout << "MyShell version " << VERSION << '\n';
 	}
+
+	namespace command
+	{
+		bool mk(const std::string& filename);
+		bool rm(const std::string& filename);
+		bool copy(const std::string& from_filename, const std::string& to_filename);
+		bool lfile(const std::string& dir_name);
+		bool mkdir(const std::string& dir_name);
+		bool rmdir(const std::string& dir_name);
+		bool pcwd();
+		bool chcwd(const std::string& dir_name);
+		bool rename(const std::string& from_name, const std::string& to_name);
+		bool tview(const std::string& filename);
+		bool bview(const std::string& filename);
+		bool version();
+		bool app(const std::string& app_name);
+		bool date();
+		bool time();
+		bool pause();
+	}
 }
 
 #endif
