d10556b8d0645a1afba5b7a841b490dd78f27031
吉田大輝 <bob181219@yahoo.co.jp>
2018-01-27 11:22:27 +0900

    Describe myshell::command::mk function

---
+++ commands.cpp
@@ -0,0 +1,34 @@
+// Standard Library
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <cctype>
+
+// Boost
+#include <boost/filesystem.hpp>
+
+// using
+using namespace std;
+using namespace boost::filesystem;
+
+bool myshell::command::mk(const string& filename)
+{
+	const path file(filename);
+	if(is_regular_file(file)) // File is found
+	{
+		cerr	<< "Warning: File \"" << filename << "\" is found.\n"
+			<< "continue?(Y/N)";
+
+		int YorN = getchar();
+		YorN = tolower(YorN);
+		if(YorN != 'y')
+			return false;
+	}
+
+	ofstream ofs(filename);
+	if(ofs.fail())
+		return false;
+	else
+		return true;
+}
